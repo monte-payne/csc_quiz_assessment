35e20ea012475a7ebb020254c03a1291cfd7183c
Monte Payne <84358400+monte-payne@users.noreply.github.com>
2021-06-27 11:56:37 +0000

    Inserted sports image

---
+++ main.py
@@ -0,0 +1,39 @@
+from tkinter import*
+from PIL import ImageTk, Image
+
+class QuizStarter:
+  def __init__(self, parent):
+    background_color="Yellow" 
+    #frame set up
+    self.quiz_frame = Frame(parent, bg = background_color, padx=125, pady=125)
+    self.quiz_frame.grid()
+
+    #Label widget for heading
+    self.heading_label = Label (self.quiz_frame, text = "SPORTS", font=("Helvetica", "30", "bold"), bg=background_color)
+    self.heading_label.grid(row=0)
+  
+    #Start button
+    self.start_button = Button (self.quiz_frame, text = "START", bg="steelblue")
+    self.start_button.grid(row=2) 
+   
+   #Exit button
+    self.exit_button = Button (self.quiz_frame, text = "EXIT", bg="Red")
+    self.exit_button.grid(row=3)
+
+    #Picture 
+    self.picture_image = Image.open("Sports.jpeg")
+    self.picture_image = self.picture_image.resize((500, 200), Image.ANTIALIAS)
+    self.picture_image = ImageTk.PhotoImage(self.picture_image)
+
+    self.image_label= Label(self.quiz_frame, image=self.picture_image)
+    self.image_label.grid(row=1)
+    
+
+
+
+#Starting point#
+if __name__ == "__main__":
+  root = Tk()
+  root.title("Sports")
+  quizStarter_object = QuizStarter(root)
+  root.mainloop()
